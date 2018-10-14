bcd3c5f16a71789ba5b755c8ba503b6e4b0d58c3
Michael Kurtinitis <mkurtinitis@ononesoftware.com>
2018-10-14 17:23:43 -0500

    first working version

--- deploy/main.js
+++ deploy/main.js
@@ -9,13 +9,16 @@
 	
 *******************************************************************************/
 
-(function ($) {
+(function ($)
+{
+
+var mLog = document.getElementById("loginfo");
 
 const kStartingPegHole = 11;
 const kPegHoleCount = 15;
-
-var mLog = document.getElementById("loginfo");
-var mJumpMap =
+const HOLE = ".";
+const PEG = "O";
+const mJumpMap =
 {
 	// This table is sorted by the "to", or destination, peg. Each table entry is a list
 	// of possible moves to the peg. For example, peg hole 6 has two possible moves:
@@ -37,6 +40,19 @@ var mJumpMap =
 	14: [{from:5,over:9,to:14}, {from:12,over:13,to:14}]
 };
 
+// This map keeps track of known peg boards. If a peg board is known to Peggy
+// it will not be searched a second time for new moves. The map is made up
+// of (key, value) pairs where key is a pegboard and value is an associated
+// game node.
+var mKnownGameStates = new Map();
+
+// The leaderboard maps final number of pegs remaining with the game state for that result.
+// From there you can chase up the tree via each game node's parent parameter. This map's
+// format is { pegsRemaining : [gameNode1, gameNode2, ...] } where gameNodes are other
+// branches of the tree that have the same number of remaining pegs. Solutions, you might say.
+var mLeaderboard = new Map();
+
+var mSolveDepth = 0;
 
 initialize();
 
@@ -53,11 +69,12 @@ function initialize()
 *******************************************************************************/	
 function logTrace(inString)
 {
+	const kMaxLines = 50;
 	var lines = mLog.value.split(/\n/).length;
 	
 	mLog.value += inString;
 
-	if (mLog.rows < lines)
+	if (mLog.rows < lines && lines <= kMaxLines)
 		mLog.rows = lines;
 }
 
@@ -73,26 +90,6 @@ function clearLog()
 /*******************************************************************************
 
 *******************************************************************************/	
-function newPegBoard(inStartingHole)
-{
-	// A peg board is an array peg locations. At each location a " " means
-	// hole, "O" means peg. Peg boards start "full" with just one zero, the
-	// starting hole.
-	outPegBoard = [];
-	for (i=0; i < kPegHoleCount; i++)
-	{
-		if (i==inStartingHole)
-			outPegBoard[i] = " ";
-		else
-			outPegBoard[i] = "O";
-	}
-	
-	return outPegBoard;
-}
-
-/*******************************************************************************
-
-*******************************************************************************/	
 function logPegBoard(inPB)
 {
 	logTrace("                   "+inPB[0]+"\n");
@@ -100,6 +97,7 @@ function logPegBoard(inPB)
 	logTrace("               "+inPB[3]+"   "+inPB[4]+"   "+inPB[5]+"\n");
 	logTrace("             "+inPB[6]+"   "+inPB[7]+"   "+inPB[8]+"   "+inPB[9]+"\n");
 	logTrace("           "+inPB[10]+"   "+inPB[11]+"   "+inPB[12]+"   "+inPB[13]+"   "+inPB[14]+"\n");
+	logTrace("\n");
 }
 
 /*******************************************************************************
@@ -119,8 +117,175 @@ function logPegBoardIndex()
 /*******************************************************************************
 
 *******************************************************************************/	
-function solve(inPegBoard)
+function PegBoard(inStartingHole)
 {
+	// A peg board is an array peg locations. At each location is either a HOLE or
+	// PEG. Peg boards start "full" of pegs with just one hole, the starting hole.
+	
+	outPegBoard = ""; // start with an empty array
+	for (i=0; i < kPegHoleCount; i++)
+	{
+		// depending on the index, insert a HOLE or PEG
+		if (i==inStartingHole)
+			outPegBoard += HOLE;
+		else
+			outPegBoard += PEG;
+	}
+	
+	return outPegBoard;
+}
+
+/*******************************************************************************
+
+*******************************************************************************/	
+function GameNode()
+{
+	var outNode = {};
+	outNode.pegBoard = null;
+	outNode.move = null;
+	outNode.parent = null;
+	outNode.children = [];
+	return outNode;
+}
+
+/*******************************************************************************
+
+*******************************************************************************/	
+function pegCount(inPegBoard)
+{
+	var numPegs = 0;
+	
+	for (i=0; i < kPegHoleCount; i++)
+	{
+		if (inPegBoard[i] == PEG)
+			numPegs += 1;
+	}
+
+	return numPegs;
+}
+
+/*******************************************************************************
+
+*******************************************************************************/	
+function updateLeaderboard(inGameNode)
+{
+	var score = pegCount(inGameNode.pegBoard);
+	
+	var nodes = mLeaderboard.get(score);
+	if (nodes == undefined)
+	{
+		// First node to have this score, add to the map
+		mLeaderboard.set(score, [inGameNode]);
+	}
+	else
+	{
+		if (!nodes.includes(inGameNode))
+			nodes.push(inGameNode);
+	}
+}
+
+/*******************************************************************************
+
+*******************************************************************************/	
+function dumpLeaderboard()
+{
+	for (score=0; score < kPegHoleCount; score++)
+	{
+		var scoreGroup = mLeaderboard.get(score);
+		if (scoreGroup != undefined)
+		{
+			logTrace(scoreGroup.length+" solutions have "+score+" pegs remaining\n");
+			if (score == 1)
+			{
+				for (nodeIdx = 0; nodeIdx < scoreGroup.length; nodeIdx++)
+				{
+					logTrace("\n");
+					logTrace("   Solution "+(nodeIdx+1)+"\n");
+					
+					var playback = [];
+					var node = scoreGroup[0];
+					while (node != null)
+					{
+						playback.unshift(node);
+						node = node.parent;	
+					}
+				
+					for (i=0;i<playback.length; i++)
+						logPegBoard(playback[i].pegBoard);
+				}
+			}
+		}
+	}
+}
+
+/*******************************************************************************
+
+*******************************************************************************/	
+function applyMove(inPegBoard, inMove)
+{
+	var outVal = inPegBoard;
+	
+	outVal = outVal.substring(0, inMove.from) + HOLE + outVal.substring(inMove.from+1);
+	outVal = outVal.substring(0, inMove.over) + HOLE + outVal.substring(inMove.over+1);
+	outVal = outVal.substring(0, inMove.to) + PEG + outVal.substring(inMove.to+1);
+
+	return outVal;
+}
+
+/*******************************************************************************
+
+*******************************************************************************/	
+function solve(inGameNode)
+{
+	mSolveDepth += 1;
+
+	var foundValidMove = false;	
+	for (var i = 0; i < kPegHoleCount; i++)
+	{
+		// Look for holes. For each hole see if  there are moves available
+		if (inGameNode.pegBoard[i] == HOLE)
+		{
+			for (var j=0; j<mJumpMap[i].length; j++)
+			{
+				var move = mJumpMap[i][j];
+				if (inGameNode.pegBoard[move.from] == PEG && inGameNode.pegBoard[move.over] == PEG && inGameNode.pegBoard[move.to] == HOLE)
+				{
+					foundValidMove = true;
+					
+					// Found a valid move. Update the peg board and set up a new game state
+					var nextBoard = applyMove(inGameNode.pegBoard.slice(0), move); 					
+					var nextNode = mKnownGameStates.get(nextBoard);
+					if (nextNode!=undefined)
+					{
+						// This board has already been traversed, just update the current
+						// game node and bail. DO NOT continue to solve!
+						inGameNode.children.push(nextNode);
+					}
+					else
+					{
+						// We have NOT seen this board yet. Add it to the map and continue to solve.
+						nextNode 			= GameNode();
+						nextNode.pegBoard	= nextBoard;
+						nextNode.move		= move;
+						nextNode.parent 	= inGameNode;
+						mKnownGameStates.set(nextNode.pegBoard, nextNode);
+						
+						inGameNode.children.push(nextNode);
+						solve(nextNode);	// down the rabbit hole!
+					}					
+				}
+			}
+		}
+	}
+
+	if (!foundValidMove)
+	{
+		// Congratulations! At this point there are no more moves for this
+		// peg board. This branch of the tree is done, put it on the leaderboard!
+		updateLeaderboard(inGameNode);
+	}
+	
+	mSolveDepth -= 1;
 }
 
 /*******************************************************************************
@@ -130,14 +295,18 @@ $('.play-button').click(function()
 {
 	clearLog();
 	logTrace("Welcome to Peggy, the peg game solver.\n");
-	
-	var pb = newPegBoard(kStartingPegHole);
+		
+	var startingGameNode = GameNode();
+	startingGameNode.pegBoard = PegBoard(kStartingPegHole);
 	
 	logTrace("\n\n");
-	logPegBoard(pb);
-	logTrace("\n");
 	logTrace("Starting board with hole at position "+kStartingPegHole+"\n");
-	solve(pb);	
+		
+	solve(startingGameNode);
+	
+	logTrace("\n\n");
+	dumpLeaderboard();
+	
 });
 
 
